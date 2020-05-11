5f56c8034ebc8cf8b33ff4b3cd46023f0dd591f5
tallam99 <tarekyallam99@gmail.com>
2020-05-11 17:16:36 -0500

    Change response to render templates

--- FNDWebsite/FND/views.py
+++ FNDWebsite/FND/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Index view, returned by the root IP or URL with basic projection description/info and form for fake news analysis
 # TODO: Make index contain the intro to the project report, input to accept text, title, and model type info, send to predict through form submission.
 def index(request):
-    return HttpResponse("Welcome to our 460J Project Website!")
+    return render(request, 'FND/index.html', {})
 
 # Prediction view, returned by form submission from index to present the user with data and selected model prediction.
 # TODO: Make prediction accept form from index, process data through pipeline, get prediction from relevant model, give info
 def prediction(request):
-    return HttpResponse("I can't predict anything yet.")
+    return render(request, 'FND/prediction.html', {})

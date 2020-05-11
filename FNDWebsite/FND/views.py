7466266b12cca852c577170bddcff32b0628b8bc
tallam99 <tarekyallam99@gmail.com>
2020-05-11 13:17:31 -0500

    Add index view

--- FNDWebsite/FND/views.py
+++ FNDWebsite/FND/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Index View (returned by the root IP or URL)
+def index(request):
+    return HttpResponse("Welcome to our 460J Project Website!")

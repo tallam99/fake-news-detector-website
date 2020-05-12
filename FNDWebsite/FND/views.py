f4358422c5088700783be0a66ab4f67b73e52955
tallam99 <tarekyallam99@gmail.com>
2020-05-11 22:54:42 -0500

    Final changes

--- FNDWebsite/FND/views.py
+++ FNDWebsite/FND/views.py
@@ -8,6 +8,8 @@ from django.templatetags.static import static
 from django.urls import reverse
 import xgboost as xgb
 import os
+import keras
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 # Index view, returned by the root IP or URL with basic projection description/info and form for fake news analysis
 # TODO: Make index contain the intro to the project report, input to accept text, title, and model type info, send to predict through form submission.
@@ -35,6 +37,7 @@ def prediction(request):
     title = data.get('title')
     text = data.get('mytext')
     df = pd.DataFrame([[title, text]], columns=['title', 'text'])
+    df_concat = pd.DataFrame([[title+text]], columns=['concat'])
 
     # preprocess data
     df_proc = process_data(df)
@@ -48,51 +51,22 @@ def prediction(request):
         model_file = '/FND/static/models/tfidf_xgb_fitted.model'
         pkl_file = '/FND/static/models/tfidf_xgb_fitted.pkl'
         model.fit_from_file(model_file, pkl_file)
-        proba = model.predict(df_proc)
+        proba = int(100*model.predict(df_proc))
     if model_name == "model2":
         model = xgb.Booster({'nthread': 4})
         cwd = os.getcwd()
         model.load_model(cwd + '/FND/static/models/numerical_xgb_eduard.model')
         proba = int(100 * model.predict(xgb.DMatrix(df_proc[ed_features].values))[0])
     if model_name == "model3":
-        pass
-    if model_name == "model4":
-        pass
-    if model_name == "model5":
-        pass
-    if model_name == "model6":
-        pass
-
-
-    # title_num_letters = df_proc['title_num_letters'].values[0]
-    # title_num_words = df_proc['title_num_words'].values[0]
-    # title_perc_up_letters = df_proc['perc_up_letters'].values[0]
-    # title_perc_up_words = df_proc['title_perc_up_words'].values[0]
-    # title_perc_nums = df_proc['title_perc_nums'].values[0]
-    # title_polarity = df_proc['title_polarity'].values[0]
-    # title_subjectivity = df_proc['title_subjectivity'].values[0]
-    # text_char_count = df_proc['text_char_count'].values[0]
-    # text_word_count = df_proc['text_word_count'].values[0]
-    # text_cap_word_count = df_proc['text_cap_word_count'].values[0]
-    # text_perc_misspells = df_proc['text_perc_misspells'].values[0]
-    # text_polarity = df_proc['text_polarity'].values[0]
-    # text_subjectivity = df_proc['text_subjectivity'].values[0]
+        cwd = os.getcwd()
+        tf1 = pickle.load(open(cwd + '/FND/static/models/feature.pkl', 'rb'))
+        tfidf = TfidfVectorizer(vocabulary=tf1.vocabulary_)
+        X = tfidf.fit_transform(df_concat)
+        model = keras.models.load_model(cwd + '/FND/static/models/DNN_please.h5')
+        proba = int(100*model.predict(X))
 
     context = {"proba": proba,
                "model": model_name_dict[model_name],
-               # "title_num_letters": title_num_letters,
-               # "title_num_words": title_num_words,
-               # "title_perc_up_letters": title_perc_up_letters,
-               # "title_perc_up_words": title_perc_up_words,
-               # "title_perc_nums": title_perc_nums,
-               # "title_polarity": title_polarity,
-               # "title_subjectivity": title_subjectivity,
-               # "text_char_count": text_char_count,
-               # "text_word_count": text_word_count,
-               # "text_cap_word_count": text_cap_word_count,
-               # "text_perc_misspells": text_perc_misspells,
-               # "text_polarity": text_polarity,
-               # "text_subjectivity": text_subjectivity,
                }
 
     # Return prediction
